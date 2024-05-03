1923fed5a8030141780119df98a611bfa40133db
nclab <nclab@DESKTOP-G3VGTUC>
2024-05-03 17:03:38 +0900

    연습

---
+++ src/main/java/com/example/web/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.example.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MemberController {
+	
+	@GetMapping("/member")
+	public void member() {
+		System.out.println("안녕");
+	}
+}
